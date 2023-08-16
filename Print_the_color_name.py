072824cc19d582270df074168ee62d190bd704ea
shyamkumarbadiganti <119720218+shyamkumarbadiganti@users.noreply.github.com>
2023-08-16 19:03:30 +0530

    c=input() if c=='V':     print('Violet') if c=='I':     print('Indigo') elif c=='B':     print('Blue') elif c=='Y':     print('Yellow') elif c=='G':     print('Green') elif c=='O':     print('Orange') elif c=='R':     print('Red') else:     print(-1)

---
+++ Print_the_color_name.py
@@ -0,0 +1,17 @@
+c=input()
+if c=='V':
+    print('Violet')
+if c=='I':
+    print('Indigo')
+elif c=='B':
+    print('Blue')
+elif c=='Y':
+    print('Yellow')
+elif c=='G':
+    print('Green')
+elif c=='O':
+    print('Orange')
+elif c=='R':
+    print('Red')
+else:
+    print(-1)
